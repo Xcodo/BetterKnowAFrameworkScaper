a7e753aed4c4d38561e4d157a024a1f2f334a12a
Mark Brett <Xcodo@users.noreply.github.com>
2018-01-17 20:50:03 +0000

    Add console project to query Better Know a Framework addresses

---
+++ src/Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Xcodo.BetterKnowAFrameworkScraper.Console
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            for (int i = 1550; i >= 1400; i--)
+            {
+                var request = $"http://{i}.pwop.me";
+                var (responseUri, responseTitle) = await GetLastUri(request);
+                await System.Console.Out.WriteLineAsync($"{i:00000} = {responseUri}: {responseTitle}");
+            }
+        }
+
+        static async Task<(string, string)> GetLastUri(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(uri, HttpCompletionOption.ResponseContentRead);
+                    response.EnsureSuccessStatusCode();
+                    string responseUri = response.RequestMessage.RequestUri.ToString();
+                    string responseTitle = GetPageTitle(await response.Content.ReadAsStringAsync());
+                    return (responseUri, responseTitle);
+                }
+                catch
+                {
+                    return (null, null);
+                }
+            }
+        }
+
+        static string GetPageTitle(string page)
+        {
+            var m = Regex.Match(page, @"<title>\s*(.+?)\s*</title>");
+            if (m.Success)
+            {
+                return m.Groups[1].Value;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
